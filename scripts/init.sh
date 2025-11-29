ae803e204fe4b695619d15a2185b85566db43aee
taka <47517002+taka1156@users.noreply.github.com>
2025-11-29 20:07:54 +0000

    1st commit

---
+++ scripts/init.sh
@@ -0,0 +1,14 @@
+mkdir packages
+mkdir packages/runtime-core
+mkdir packages/runtime-dom
+
+## core
+touch packages/runtime-core/index.ts
+touch packages/runtime-core/apiCreateApp.ts
+touch packages/runtime-core/component.ts
+touch packages/runtime-core/componentOptions.ts
+touch packages/runtime-core/renderer.ts
+
+## dom
+touch packages/runtime-dom/index.ts
+touch packages/runtime-dom/nodeOps.ts
